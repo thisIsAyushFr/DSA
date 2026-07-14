8c1f6df020b4921859dc815df277d4559c042bde
Ayush <ayushrollno5@gmail.com>
2026-07-14 23:27:05 +0530

    Time: 1 ms (95%), Space: 46.5 MB (32%) - LeetHub

--- 3978-unique-middle-element/3978-unique-middle-element.java
+++ 3978-unique-middle-element/3978-unique-middle-element.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isMiddleElementUnique(int[] nums) {
-        int m = nums[nums.length / 2];  // middle element
+        int m = nums[nums.length / 2];
         int c = 0;
         for (int i : nums) {
             if (m == i) {
