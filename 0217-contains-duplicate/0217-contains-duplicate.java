ae967c6bca3a54510c32a4f890f0f460f01a6ac2
Ayush <ayushrollno5@gmail.com>
2026-07-20 13:22:34 +0530

    Time: 22 ms (30.82%), Space: 108.2 MB (16.28%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for(int i : nums){
+            if(seen.contains(i)){
+                return true;
+            }
+            seen.add(i);
+        }
+        return false;
+    }
+}
